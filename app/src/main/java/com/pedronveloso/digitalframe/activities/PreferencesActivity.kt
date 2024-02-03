268f98615b85c741c8a39216fb2087eeea86747c
Pedro Veloso <pedro.n.veloso@gmail.com>
2024-02-02 17:28:34 -0800

    Small preferences cleanup

--- app/src/main/java/com/pedronveloso/digitalframe/activities/PreferencesActivity.kt
+++ app/src/main/java/com/pedronveloso/digitalframe/activities/PreferencesActivity.kt
@@ -1,32 +1,29 @@
 package com.pedronveloso.digitalframe.activities
 
 import android.content.Context
-import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import android.os.Bundle
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.pedronveloso.digitalframe.elements.PhotosBackgroundViewModel.Companion.BACKGROUND_PHOTOS_DIR
 import com.pedronveloso.digitalframe.ui.MyTypography
 import java.io.File
@@ -36,20 +33,16 @@ import kotlin.math.max
 
 class PreferencesActivity : ComponentActivity() {
 
-    private lateinit var imagePickerLauncher: ActivityResultLauncher<Intent>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PreferencesScreen()
         }
-
     }
 
     @Composable
     fun PreferencesScreen() {
         val context = LocalContext.current
-        val uris = remember { mutableStateListOf<Uri>() }
         var showImagePicker by remember { mutableStateOf(false) }
 
 

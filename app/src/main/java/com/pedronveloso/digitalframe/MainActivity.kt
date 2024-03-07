174cbe335da309b1ce445bc5da9657571aba2787
Pedro Veloso <pedro.n.veloso@gmail.com>
2024-03-06 23:00:44 -0800

    New attempt at enforcing fullscreen immersive mode on certain devices

--- app/src/main/java/com/pedronveloso/digitalframe/MainActivity.kt
+++ app/src/main/java/com/pedronveloso/digitalframe/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.pedronveloso.digitalframe
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -58,8 +61,19 @@ import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    // Optional: Re-enter immersive mode after user interaction
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            onCreateToggleImmersiveMode()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        onCreateToggleImmersiveMode()
 
         // Keep Screen On.
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
@@ -79,6 +93,22 @@ class MainActivity : ComponentActivity() {
                         countdownViewModel = viewModel(),
                     )
                 }
+            }
+        }
+    }
+
+    private fun onCreateToggleImmersiveMode() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        } else {
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
         }
     }

4037ca11ba2298a90654a074b8d97e64ea58cac7
Pedro Veloso <pedro.n.veloso@gmail.com>
2024-03-30 15:44:09 -0700

    Add logs for photo erase operation

--- app/src/main/java/com/pedronveloso/digitalframe/elements/background/BackgroundPhotosEraser.kt
+++ app/src/main/java/com/pedronveloso/digitalframe/elements/background/BackgroundPhotosEraser.kt
@@ -5,10 +5,13 @@ import android.content.Context
 import android.widget.Toast
 import com.pedronveloso.digitalframe.R
 import com.pedronveloso.digitalframe.elements.background.PhotosBackgroundViewModel.Companion.BACKGROUND_PHOTOS_DIR
+import com.pedronveloso.digitalframe.utils.log.LogStoreProvider
 import java.io.File
 
 
 class BackgroundPhotosEraser(private val context: Context) {
+
+    private val logger = LogStoreProvider.getLogStore()
 
     /**
      * Function to show the confirmation dialog and delete photos.
@@ -32,11 +35,14 @@ class BackgroundPhotosEraser(private val context: Context) {
     }
 
     private fun deleteBackgroundPhotosDirectory() {
+        logger.log("Attempting to delete background photos directory contents.")
         val backgroundPhotosDir = File(context.filesDir, BACKGROUND_PHOTOS_DIR)
         if (backgroundPhotosDir.exists()) {
             backgroundPhotosDir.deleteRecursively()
+            logger.log("Background photos directory contents deleted.")
             Toast.makeText(context, R.string.pref_bg_erase_success, Toast.LENGTH_SHORT).show()
         } else {
+            logger.log("Background photos directory is empty.")
             Toast.makeText(context, R.string.pref_bg_erase_failed_empty, Toast.LENGTH_SHORT).show()
         }
     }

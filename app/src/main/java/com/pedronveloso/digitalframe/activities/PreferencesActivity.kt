358c2ed9e27f73505fb53db79a16617efe9118a1
Pedro Veloso <pedro.n.veloso@gmail.com>
2024-03-29 17:31:52 -0700

    Make save button display a checkmark upon save

--- app/src/main/java/com/pedronveloso/digitalframe/activities/PreferencesActivity.kt
+++ app/src/main/java/com/pedronveloso/digitalframe/activities/PreferencesActivity.kt
@@ -1,22 +1,11 @@
 package com.pedronveloso.digitalframe.activities
 
-import android.Manifest
-import android.annotation.SuppressLint
 import android.app.DatePickerDialog
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Looper
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,8 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
@@ -35,11 +22,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
@@ -54,14 +39,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -79,16 +61,15 @@ import com.pedronveloso.digitalframe.preferences.InputType
 import com.pedronveloso.digitalframe.preferences.PreferenceItem
 import com.pedronveloso.digitalframe.preferences.PreferencesRoot
 import com.pedronveloso.digitalframe.preferences.PreferencesSection
-import com.pedronveloso.digitalframe.preferences.location.LocationData
 import com.pedronveloso.digitalframe.ui.DigitalFrameTheme
 import com.pedronveloso.digitalframe.ui.MyTypography
+import com.pedronveloso.digitalframe.ui.preferences.LocationPreferenceComposable
 import java.text.SimpleDateFormat
 import java.time.MonthDay
 import java.time.Year
 import java.time.ZoneId
 import java.util.Calendar
 import java.util.Locale
-import java.util.concurrent.Executor
 
 
 class PreferencesActivity : ComponentActivity() {
@@ -182,7 +163,8 @@ class PreferencesActivity : ComponentActivity() {
                 type = InputType.DATE,
                 initialValueProvider = { countdownData.getTargetDate().toString() },
                 onChangeCallback = { value ->
-                    val dateValue = SimpleDateFormat("MMM d, yyyy", Locale.getDefault()).parse(value)
+                    val dateValue =
+                        SimpleDateFormat("MMM d, yyyy", Locale.getDefault()).parse(value)
                     countdownData.setTargetDate(
                         dateValue.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
                     )
@@ -443,9 +425,9 @@ fun InputFieldPreferenceComposable(preference: PreferenceItem.InputFieldPref) {
                 placeholder = { Text(preference.hint ?: "") },
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
                 keyboardActions =
-                    KeyboardActions(onDone = {
-                        preference.onChangeCallback?.invoke(text)
-                    }),
+                KeyboardActions(onDone = {
+                    preference.onChangeCallback?.invoke(text)
+                }),
                 singleLine = true,
                 modifier =
                 Modifier
@@ -466,7 +448,10 @@ fun InputFieldPreferenceComposable(preference: PreferenceItem.InputFieldPref) {
                                 set(year, month, dayOfMonth)
                             }
                         text =
-                            SimpleDateFormat("MMM d, yyyy", Locale.getDefault()).format(calendar.time)
+                            SimpleDateFormat(
+                                "MMM d, yyyy",
+                                Locale.getDefault()
+                            ).format(calendar.time)
                         preference.onChangeCallback?.invoke(text)
                     }, Year.now().value, MonthDay.now().monthValue - 1, MonthDay.now().dayOfMonth)
                 }
@@ -520,170 +505,3 @@ fun ButtonPreferenceComposable(preference: PreferenceItem.Button) {
         Text(preference.label)
     }
 }
-
-
-@SuppressLint("MissingPermission")
-@Composable
-fun LocationPreferenceComposable(preference: PreferenceItem.LocationPref) {
-    val locationManager = LocalContext.current.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-    val initialLocation = preference.initialValueProvider.invoke()
-    var latitude by remember { mutableStateOf(initialLocation.latitude.toString()) }
-    var longitude by remember { mutableStateOf(initialLocation.longitude.toString()) }
-    var latError by remember { mutableStateOf(false) }
-    var lonError by remember { mutableStateOf(false) }
-    var isLoading by remember { mutableStateOf(false) }
-    val context = LocalContext.current
-    val executor = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-        LocalContext.current.mainExecutor
-    } else {
-        ContextCompat.getMainExecutor(context)
-    }
-
-    // Permission handling
-    val locationPermissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission(),
-        onResult = { isGranted: Boolean ->
-            if (isGranted) {
-                getCurrentLocation(locationManager, executor, onLocationReceived = { location ->
-                    latitude = location.latitude.toString()
-                    longitude = location.longitude.toString()
-                    isLoading = false
-                })
-            }
-        }
-    )
-
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = preference.title, style = MyTypography.titleMedium)
-        preference.description?.let {
-            Spacer(modifier = Modifier.height(6.dp))
-            Text(text = it, style = MyTypography.bodyLarge)
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = latitude,
-            onValueChange = {
-                latitude = it
-                latError = !isValidLatitude(it)
-            },
-            isError = latError,
-            label = { stringResource(id = R.string.pref_location_lat) },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-        if (latError) {
-            Text(
-                stringResource(id = R.string.pref_location_lat_error),
-                color = MaterialTheme.colorScheme.error
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        TextField(
-            value = longitude,
-            onValueChange = {
-                longitude = it
-                lonError = !isValidLongitude(it)
-            },
-            isError = lonError,
-            label = { Text(stringResource(id = R.string.pref_location_lon)) },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
-        )
-        if (lonError) {
-            Text(
-                stringResource(id = R.string.pref_location_lon_error),
-                color = MaterialTheme.colorScheme.error
-            )
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(
-                onClick = {
-                    if (!latError && !lonError) {
-                        preference.onChangeCallback?.invoke(
-                            LocationData(
-                                latitude.toDouble(),
-                                longitude.toDouble()
-                            )
-                        )
-                    }
-                },
-                enabled = !latError && !lonError
-            ) {
-                Text(stringResource(id = R.string.pref_location_save))
-            }
-
-            Spacer(modifier = Modifier.height(8.dp).width(8.dp))
-
-            Button(onClick = {
-
-                when (PackageManager.PERMISSION_GRANTED) {
-                    ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                        isLoading = true
-                        getCurrentLocation(locationManager, executor, onLocationReceived = { location ->
-                            latitude = location.latitude.toString()
-                            longitude = location.longitude.toString()
-                            isLoading = false
-                        })
-                    }
-                    else -> locationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                }
-            }) {
-                if (isLoading) {
-                    CircularProgressIndicator(modifier = Modifier.size(16.dp), color = Color.White)
-                } else {
-                    Text("Get current")
-                }
-            }
-        }
-    }
-}
-
-@SuppressLint("MissingPermission")
-private fun getCurrentLocation(
-    locationManager: LocationManager,
-    executor: Executor,
-    onLocationReceived: (Location) -> Unit
-) {
-    var locationListener: LocationListener? = null
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        locationManager.getCurrentLocation(
-            LocationManager.GPS_PROVIDER,
-            null,
-            executor
-        ) { location ->
-            location?.let {
-                onLocationReceived(it)
-            }
-        }
-    } else {
-        locationListener = LocationListener { location ->
-            onLocationReceived(location)
-            locationListener?.let { locationManager.removeUpdates(it) }
-        }
-
-
-        locationManager.requestSingleUpdate(
-            LocationManager.GPS_PROVIDER,
-            locationListener,
-            Looper.getMainLooper()
-        )
-
-    }
-}
-
-fun isValidLatitude(value: String): Boolean =
-    value.toDoubleOrNull()?.let { it in -90.0..90.0 } ?: false
-
-fun isValidLongitude(value: String): Boolean =
-    value.toDoubleOrNull()?.let { it in -180.0..180.0 } ?: false

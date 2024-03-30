12888c9be2f1dc0a0de19d14333e2393519878e8
Pedro Veloso <pedro.n.veloso@gmail.com>
2024-03-29 22:37:28 -0700

    Limit number of in-memory logs to 400 entries, FIFO

--- app/src/main/java/com/pedronveloso/digitalframe/utils/log/InMemoryLogStore.kt
+++ app/src/main/java/com/pedronveloso/digitalframe/utils/log/InMemoryLogStore.kt
@@ -2,20 +2,28 @@ package com.pedronveloso.digitalframe.utils.log
 
 import android.util.Log
 import java.time.LocalDateTime
+import java.util.LinkedList
 
 class InMemoryLogStore : LogStore {
 
-    private val logs = mutableListOf<LogEntry>()
+    private val logs = LinkedList<LogEntry>()
 
     override fun log(message: String) {
         val callingClass = determineCallingClass()
-        logs.add(LogEntry(callingClass, message, LocalDateTime.now(), LogLevel.INFO))
+        addLogEntry(LogEntry(callingClass, message, LocalDateTime.now(), LogLevel.INFO))
         Log.v(callingClass, message)
     }
 
     override fun logError(message: String, throwable: Throwable?) {
         val callingClass = determineCallingClass()
-        logs.add(LogEntry(callingClass, "$message: ${throwable?.message}", LocalDateTime.now(), LogLevel.ERROR))
+        addLogEntry(
+            LogEntry(
+                callingClass,
+                "$message: ${throwable?.message}",
+                LocalDateTime.now(),
+                LogLevel.ERROR
+            )
+        )
         Log.e(callingClass, message, throwable)
     }
 
@@ -23,10 +31,20 @@ class InMemoryLogStore : LogStore {
         return logs.toList()
     }
 
+    private fun addLogEntry(logEntry: LogEntry) {
+        if (logs.size >= MAX_LOG_ENTRIES) {
+            logs.removeFirst()
+        }
+        logs.add(logEntry)
+    }
 
     private fun determineCallingClass(): String {
         val stackTrace = Thread.currentThread().stackTrace
         val callingClass = stackTrace[3].className
         return callingClass
+    }
+
+    companion object {
+        private const val MAX_LOG_ENTRIES = 400
     }
 }
